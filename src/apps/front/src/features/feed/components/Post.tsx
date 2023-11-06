b9562cc57b665611cf9eff4278935e267c7c72ca
LUH <dldmlgus0120@gmail.com>
2023-11-06 16:05:51 +0900

    Post map함수 적용 css도 적용

--- src/apps/front/src/features/feed/components/Post.tsx
+++ src/apps/front/src/features/feed/components/Post.tsx
@@ -1,21 +1,46 @@
+import React from 'react';
+
 const Post = () => {
+  const posts = [
+    {
+      image: './img/card-img.png',
+      profileImage: './img/Rectangle 17.png',
+      username: 'leechi',
+      likeCount: 23,
+    },
+    {
+      image: './img/card-img.png',
+      profileImage: './img/Rectangle 17.png',
+      username: 'leechi',
+      likeCount: 23,
+    },
+    {
+      image: './img/card-img.png',
+      profileImage: './img/Rectangle 17.png',
+      username: 'leechi',
+      likeCount: 23,
+    },
+  ];
+
   return (
     <div className="feed-cards">
-      <div className="card">
-        <div className="card-img">
-          <img src="./img/card-img.png" alt="" />
-        </div>
-        <div className="card-bottom">
-          <div>
-            <img src="./img/Rectangle 17.png" alt="" />
-            <span>leechi</span>
+      {posts.map((post, index) => (
+        <div className="card" key={index}>
+          <div className="card-img">
+            <img src={post.image} alt="" />
           </div>
-          <div>
-            <img src="./img/like.png" alt="" />
-            <span>23</span>
+          <div className="card-bottom">
+            <div>
+              <img src={post.profileImage} alt="" />
+              <span>{post.username}</span>
+            </div>
+            <div>
+              <img src="./img/like.png" alt="" />
+              <span>{post.likeCount}</span>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
