ee06a2b9bac2f664e9b39c4c95845858339c265a
김유원 <gim-yuwon@gim-yuwon-ui-MacBookAir.local>
2023-11-07 16:07:46 +0900

    모달창 기본 구상

--- src/apps/front/src/features/createList/component/BoardModal.tsx
+++ src/apps/front/src/features/createList/component/BoardModal.tsx
@@ -15,9 +15,36 @@ const BoardModal: React.FC<BoardModalProps> = ({ onClose }) => {
   return (
     <div className="board-modal-black-bg" onClick={ModalBgClose}>
       <div className="board-modal-bg">
-        <button onClick={onClose}>닫기</button>
+        <button className="Modal-close-button" onClick={onClose}>
+          ❌
+        </button>
         {/* 모달 내용을 원하는 대로 추가 */}
-        <h2>모달 제목</h2>
+        <div className="Modal-list">
+          <input className="Modla-select-button" type="radio" />
+          <div className="Modal-list-img">+</div>
+          <div className="Modal-prompt-list">
+            <div className="Modal-prompt-positive">
+              <h4>Prompt</h4>
+              <p>
+                hyper-detailed front medium shot of Black short-haired girl,
+                front view, cente r composition, dress shirt, by loish, trending
+                on artstation hd, photorealistic anime girl render, artgee m,
+                highly detailed face, digital art, impressionism,deep gaze,
+                school background
+              </p>
+            </div>
+            <div className="Modal-prompt-negative">
+              <h4>Negative Prompt</h4>
+              <p>
+                hyper-detailed front medium shot of Black short-haired girl,
+                front view, cente r composition, dress shirt, by loish, trending
+                on artstation hd, photorealistic anime girl render, artgee m,
+                highly detailed face, digital art, impressionism,deep gaze,
+                school background
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
