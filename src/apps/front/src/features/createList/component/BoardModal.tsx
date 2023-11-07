398e505f07a658bf7ea14772ca333c54c71e35d1
김유원 <gim-yuwon@gim-yuwon-ui-MacBookAir.local>
2023-11-07 15:42:43 +0900

    모달창 배경클릭시 닫기 완성

--- src/apps/front/src/features/createList/component/BoardModal.tsx
+++ src/apps/front/src/features/createList/component/BoardModal.tsx
@@ -1,13 +1,21 @@
-const BoardModal = (a) => {
+import React from 'react';
+
+interface BoardModalProps {
+  onClose: () => void;
+}
+
+const BoardModal: React.FC<BoardModalProps> = ({ onClose }) => {
+  const ModalBgClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.target === e.currentTarget) {
+      // 배경 클릭 시 모달을 닫음
+      onClose();
+    }
+  };
+
   return (
-    <div
-      className="board-modal-black-bg"
-      onClick={() => {
-        console.log(1);
-      }}
-    >
+    <div className="board-modal-black-bg" onClick={ModalBgClose}>
       <div className="board-modal-bg">
-        <button onClick={a.onClose}>닫기</button>
+        <button onClick={onClose}>닫기</button>
         {/* 모달 내용을 원하는 대로 추가 */}
         <h2>모달 제목</h2>
       </div>
