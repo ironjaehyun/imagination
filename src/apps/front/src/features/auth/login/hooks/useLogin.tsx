5ac796292001b175e27c5f90dc87c85ce839594a
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-07 12:05:53 +0900

    feat(login): Add token cookie and logout

--- src/apps/front/src/features/auth/login/hooks/useLogin.tsx
+++ src/apps/front/src/features/auth/login/hooks/useLogin.tsx
@@ -1,28 +1,33 @@
 import axios from '../../api/auth';
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const useLogin = () => {
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
   const [cookies, setCookie, removeCookie] = useCookies(['token']);
+  const navigate = useNavigate();
   const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     axios.post('/', { id: id, password: pw }).then((res) => {
       const newToken = res.data.token;
 
       setCookie('token', newToken, { path: '/' });
+      cookies.token ? navigate('/feed') : null;
     });
   };
 
-  axios.get('/api/some-protected-r');
+  const handleLogout = () => {
+    removeCookie('token');
+  };
 
   return {
     handleLogin,
     setId,
     setPw,
     cookies,
-    removeCookie,
+    handleLogout,
   };
 };
 
