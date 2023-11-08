1543cced4cc28c061657ca8bcc81ebc7f830aae2
jaehyun <kjh47515719@gmail.com>
2023-11-08 17:27:48 +0900

    feat:chat 초대박스 타이틀 new messages, 닫기버튼 배치완료

--- src/apps/front/src/features/chat/components/ChatInvite.tsx
+++ src/apps/front/src/features/chat/components/ChatInvite.tsx
@@ -1,7 +1,12 @@
 const ChatInvite = () => {
   return (
     <div className="chat-invite-bg">
-      <div className="chat-invite-box"></div>
+      <div className="chat-invite-box">
+        <div className="chat-invite-title">
+          <span>New Messages</span>
+          <img src="../chatimg/close.svg" />
+        </div>
+      </div>
     </div>
   );
 };
