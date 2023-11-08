1c51de32faff193a723fe03f85aa01d90ef37ba6
jaehyun <kjh47515719@gmail.com>
2023-11-08 17:40:32 +0900

    feat:chat 초대박스 버튼 생성

--- src/apps/front/src/features/chat/components/ChatInvite.tsx
+++ src/apps/front/src/features/chat/components/ChatInvite.tsx
@@ -10,6 +10,12 @@ const ChatInvite = () => {
         <div className="chat-invite-search">
           <input></input>
         </div>
+
+        <div className="chat-invite-list"></div>
+
+        <div className="chat-invite-btn">
+          <button>Chat</button>
+        </div>
       </div>
     </div>
   );
