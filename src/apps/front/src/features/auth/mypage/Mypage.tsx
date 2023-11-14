25fce15db452d76f1f545506df56988853bff58a
leechi <leechi2228@gmail.com>
2023-11-14 16:34:25 +0900

    style(mypage): Add list-tab style

--- src/apps/front/src/features/auth/mypage/Mypage.tsx
+++ src/apps/front/src/features/auth/mypage/Mypage.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import MypageInfo from './components/MypageInfo';
 import Gnb from '../../shared/Gnb';
 import EditUser from './components/EditUser';
@@ -7,25 +6,23 @@ import useMypage from './hooks/useMypage';
 const Mypage = () => {
   const { myPageModal, handleEditModalClose, handleEditModalOpen } =
     useMypage();
-  console.log(myPageModal);
-  const [id] = useState<string>('hi');
+
   return (
     <div>
-      <div className="page">
+      <div className="mypage-box">
         <Gnb />
         {myPageModal && <EditUser close={handleEditModalClose} />}
         <div className="mypage">
           <MypageInfo open={handleEditModalOpen} />
-          <hr />
-          <ul>
-            <li>posts</li>
-            <li>like</li>
-            <li>save</li>
-          </ul>
-          <footer className="mypage__footer">
-            <h1>{id}</h1>
-            {/* post가 클릭되면 post관련 나오고 like 나오고 등등등 나온다. */}
-          </footer>
+          <hr className="mypage-line" />
+          <div className="mypage-tab">
+            <ul className="mypage-tab-list">
+              <li>게시물</li>
+              <li>좋아요</li>
+              <li>저장된 이미지</li>
+            </ul>
+          </div>
+          <div className="mypage__footer"></div>
         </div>
       </div>
     </div>
