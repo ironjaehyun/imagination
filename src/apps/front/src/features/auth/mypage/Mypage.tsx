a7f0f1e27441d6e94f0b7170722aec147bbaaa88
leechi <leechi2228@gmail.com>
2023-11-14 17:40:44 +0900

    feat(mypage): Add mypage tab

--- src/apps/front/src/features/auth/mypage/Mypage.tsx
+++ src/apps/front/src/features/auth/mypage/Mypage.tsx
@@ -2,10 +2,12 @@ import MypageInfo from './components/MypageInfo';
 import Gnb from '../../shared/Gnb';
 import EditUser from './components/EditUser';
 import useMypage from './hooks/useMypage';
+import { useState } from 'react';
 
 const Mypage = () => {
   const { myPageModal, handleEditModalClose, handleEditModalOpen } =
     useMypage();
+  const [clickTab, setClickTab] = useState(0);
 
   return (
     <div>
@@ -17,12 +19,79 @@ const Mypage = () => {
           <hr className="mypage-line" />
           <div className="mypage-tab">
             <ul className="mypage-tab-list">
-              <li>게시물</li>
-              <li>좋아요</li>
-              <li>저장된 이미지</li>
+              <li
+                onClick={() => {
+                  setClickTab(0);
+                }}
+              >
+                게시물
+              </li>
+              <li
+                onClick={() => {
+                  setClickTab(1);
+                }}
+              >
+                좋아요
+              </li>
+              <li
+                onClick={() => {
+                  setClickTab(2);
+                }}
+              >
+                저장된 이미지
+              </li>
             </ul>
           </div>
-          <div className="mypage__footer"></div>
+          <div className="mypage-footer">
+            {clickTab === 0 ? (
+              <section className="mypage-posts">
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+              </section>
+            ) : null}
+            {clickTab === 1 ? (
+              <section className="mypage-likes">
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+              </section>
+            ) : null}
+            {clickTab === 2 ? (
+              <section className="mypage-images">
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
+                  alt=""
+                />
+              </section>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
