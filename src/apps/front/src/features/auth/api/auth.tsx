715c2204c51d514322901d8c3538995dce8ba56a
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-14 12:48:15 +0900

    Fix(imagination): Fix Cors Error

--- src/apps/front/src/features/auth/api/auth.tsx
+++ src/apps/front/src/features/auth/api/auth.tsx
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import { PORT } from '../../../../../../packages/models/port';
 
-axios.defaults.withCredentials = true;
-
 const instance = axios.create({
   baseURL: `http://localhost:${PORT}`,
   headers: {
     accept: 'application/json',
     credentials: 'include',
   },
+  withCredentials: true,
 });
 
 export default instance;
