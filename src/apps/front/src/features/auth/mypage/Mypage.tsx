06aca38fd334d0109be0ff33a9d6619fd683504f
leechi <leechi2228@gmail.com>
2023-11-14 18:59:10 +0900

    feat(mypage): Add imgData test

--- src/apps/front/src/features/auth/mypage/Mypage.tsx
+++ src/apps/front/src/features/auth/mypage/Mypage.tsx
@@ -14,6 +14,50 @@ const Mypage = () => {
     handleCheckTab,
   } = useMypage();
 
+  const imageData = [
+    {
+      post_id: 1,
+      img: 'https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph',
+    },
+    {
+      post_id: 2,
+      img: 'https://images.unsplash.com/photo-1484542603127-984f4f7d14cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      post_id: 3,
+      img: 'https://i.pinimg.com/originals/d2/11/77/d21177861d4c3ee30af8ee649a033b91.jpg',
+    },
+    {
+      post_id: 4,
+      img: 'https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148901163.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697500800&semt=sph',
+    },
+  ];
+  const likeData = [
+    {
+      post_id: 1,
+      img: 'https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph',
+    },
+    {
+      post_id: 2,
+      img: 'https://images.unsplash.com/photo-1484542603127-984f4f7d14cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ];
+
+  const savedImgData = [
+    {
+      post_id: 1,
+      img: 'https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph',
+    },
+    {
+      post_id: 2,
+      img: 'https://images.unsplash.com/photo-1484542603127-984f4f7d14cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      post_id: 3,
+      img: 'https://i.pinimg.com/originals/d2/11/77/d21177861d4c3ee30af8ee649a033b91.jpg',
+    },
+  ];
+
   return (
     <div>
       <div className="mypage-box">
@@ -21,7 +65,9 @@ const Mypage = () => {
         {myPageModal && <EditUser close={handleEditModalClose} />}
         <div className="mypage">
           <MypageInfo open={handleEditModalOpen} />
-          <hr className="mypage-line" />
+          <div className="mypage-line-box">
+            <hr className="mypage-line" />
+          </div>
           <div className="mypage-tab">
             <ul className="mypage-tab-list">
               <li
@@ -59,38 +105,23 @@ const Mypage = () => {
           <div className="mypage-footer">
             {clickTab === 0 ? (
               <section className="mypage-posts">
-                <img
-                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
-                  alt=""
-                />
-                <img
-                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
-                  alt=""
-                />
-                <img
-                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
-                  alt=""
-                />
+                {imageData.map((img) => {
+                  return <img src={img.img} key={img.post_id} />;
+                })}
               </section>
             ) : null}
             {clickTab === 1 ? (
               <section className="mypage-likes">
-                <img
-                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
-                  alt=""
-                />
+                {likeData.map((img) => {
+                  return <img src={img.img} key={img.post_id} />;
+                })}
               </section>
             ) : null}
             {clickTab === 2 ? (
               <section className="mypage-images">
-                <img
-                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
-                  alt=""
-                />
-                <img
-                  src="https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph"
-                  alt=""
-                />
+                {savedImgData.map((img) => {
+                  return <img src={img.img} key={img.post_id} />;
+                })}
               </section>
             ) : null}
           </div>
