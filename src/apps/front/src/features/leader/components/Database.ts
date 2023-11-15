cc6febb1e15bf3004691effa42bf7b13f5c039ab
dreambear00 <dreambear00@naver.com>
2023-11-15 12:18:16 +0900

    feat:leader Database

---
+++ src/apps/front/src/features/leader/components/Database.ts
@@ -0,0 +1,178 @@
+export interface LeaderboardEntry {
+  name: string;
+  grade: string;
+  score: number;
+  img: string;
+  dt: string;
+}
+
+export const Leaderboard: LeaderboardEntry[] = [
+  {
+    name: 'Shawn Anna',
+    grade: 'Beginner',
+    score: 50,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Mb%2FMb8jCxgK5qNPTejCWoRe2XIC0zx2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-2f91df37-7b69-4f7b-b796-f4167120f777.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Fidel And',
+    grade: 'Beginner',
+    score: 40,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/2N%2F2NWwFrGFpAgK8L8BxXccewUAD9d2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtdmoyazQ%3D-0c7d3019-06cd-45c6-80f7-103aa9ddaeaa.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Essie Hickle',
+    grade: 'Expert',
+    score: 30,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Mb%2FMb8jCxgK5qNPTejCWoRe2XIC0zx2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-d76b5392-1710-4bee-b8b3-9ad41f8b74ee.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Ella Nasch',
+    grade: 'Expert',
+    score: 200,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/js%2FjsR8anQkJteCZtTt9uDzaL1GwKG3%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNDI1bGQ%3D-0c03ad18-8e03-4b16-86ae-17b2f5e90c86.webp?alt=media',
+    dt: '2022-11-14',
+  },
+  {
+    name: 'Della Ansch',
+    grade: 'Designer',
+    score: 1000,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/m6%2Fm6ovHLTEtEdFxmv9lix7ZFqMF7j1%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-f6711984-759a-4c0b-9ffd-ab3c43bc2835.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Lola Anha',
+    grade: 'Designer',
+    score: 3000,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Nw%2FNwHefuEEpObQ61fundUTfQbH1Z73%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-89ce23f4-65cd-48ec-8bd1-3944814f0f94.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Aola Hada',
+    grade: 'Designer',
+    score: 100,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/W9%2FW9s072cWtqVJ7YXR7XsXlhoqNIG2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtdmoyazQ%3D-2c8a5feb-d675-4802-a164-146df815bdaa.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Pado Zoha',
+    grade: 'Master',
+    score: 800,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/dX%2FdXzefbfMvoSN892DGzzvky5RTek2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtZ25oazI%3D-549fef8f-a11e-41f1-b7ea-d37727fc0983.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Dado Poha',
+    grade: 'Master',
+    score: 600,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/4F%2F4F2ypQNKNnVELWKcka4DDLPxapJ3%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtdmoyazQ%3D-8fb66cc9-70a2-4153-97ab-d34838f905b4.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Cado Coha',
+    grade: 'Master',
+    score: 700,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/1F%2F1FfFlAjwfIMxkL7rWUP4pDj0mqy2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNDI1bGQ%3D-9762f821-6115-450c-a02e-11eaf814630b.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Cowo Moha',
+    grade: 'Master',
+    score: 2000,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/z3%2Fz3ymSa8lntTYwx1ONRCMzGvMAIv1%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNDI1bGQ%3D-bc8748bb-1d50-43ce-ad0a-2fefab8239b4.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Nado Noha',
+    grade: 'Master',
+    score: 900,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/vt%2FvtdFsYuNIFUl84H3SgKKWgLs0OA3%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtOWtzcXo%3D-6d04a9cd-7118-4fa8-87bc-5c4e0b9e6e04.webp?alt=media',
+    dt: '2022-11-11',
+  },
+  {
+    name: 'Pado Mua',
+    grade: 'Master',
+    score: 4000,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/mo%2FmoupCElIc4OeUrlDStsy5lj5OlE2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtbWhuODY%3D-988d93b4-e4a5-4d82-84c7-65c69c7a6b4a.webp?alt=media',
+    dt: '2022-11-14',
+  },
+  {
+    name: 'Lena Müller',
+    grade: 'Beginner',
+    score: 300,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/ZF%2FZFWS7bnwAwaQoDlLrHOKMQH81Lt1%2Fgenerated%2F33%2F330f2215-2063-44ba-8874-f863ac5563c3.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Ivan Petrov',
+    grade: 'Beginner',
+    score: 500,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Zj%2FZj5Hk6u4qwcEq1mmpKmcBnd5HQB3%2Fgenerated%2F09%2F0995b929-aa1a-48f8-a1f0-ffba0e66afdd.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Yuki Tanaka',
+    grade: 'Beginner',
+    score: 60,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYta2ZxajI%3D-3a9d6496-7c81-4dd5-9486-ea733e5ccdbc.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Mia Rossi',
+    grade: 'Beginner',
+    score: 400,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYta2tuZDU%3D-eedf6415-69c5-476d-97f0-a334bb8dc7f5.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Chen Wei',
+    grade: 'Beginner',
+    score: 70,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtZ25oazI%3D-f71faf43-45d3-4bfa-b780-ecd3fc7a3e04.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Anika Patel',
+    grade: 'Beginner',
+    score: 80,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/rU%2FrUbpEh62J2esf47YzsY3Tk81ySe2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtaGQ3Zjk%3D-950c4a48-4b91-4026-a18a-6475aa3ee661.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Oliver Smith',
+    grade: 'Beginner',
+    score: 90,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/We%2FWezmS86BcrchhuFXi6XvvnUCZcz1%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNjY3eGM%3D-1695e053-41b1-428e-805a-6fdba7f58a33.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Liam Nguyen',
+    grade: 'Beginner',
+    score: 20,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtbWhuODY%3D-21fb60d7-7731-4886-bec7-eb3bca26438e.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Sophia Martinez',
+    grade: 'Beginner',
+    score: 10,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/hW%2FhWbxksorq3P7mQ6T0iQmdwyebdC3%2Fgenerated%2F00%2F0094e9dc-4cf8-4712-bcca-9194d171acc5.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Taphia Artinez',
+    grade: 'Beginner',
+    score: 5,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/We%2FWezmS86BcrchhuFXi6XvvnUCZcz1%2Fgenerated%2Fc5%2Fc5ca0b9f-3e98-4bd6-9e5c-3d25dc59715c.webp?alt=media',
+    dt: '2023-11-14',
+  },
+  {
+    name: 'Mophia Ratinez',
+    grade: 'Beginner',
+    score: 3,
+    img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/ge%2Fgexqq7FTmgPlkBsg3oKfGEO677e2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtcHdrNHY%3D-563a340e-f8d7-49e0-a221-539755cc2217.webp?alt=media',
+    dt: '2023-11-14',
+  },
+];
