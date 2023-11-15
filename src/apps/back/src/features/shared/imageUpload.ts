5038cb370b2785a98b6330d70655314978bb29c9
leechi <leechi2228@gmail.com>
2023-11-15 14:46:18 +0900

    feat: Add env setting

--- src/apps/back/src/features/shared/imageUpload.ts
+++ src/apps/back/src/features/shared/imageUpload.ts
@@ -5,16 +5,16 @@ import multerS3 from 'multer-s3';
 const s3 = new S3Client({
   region: 'ap-northeast-2',
   credentials: {
-    accessKeyId: 'IAM에서 발급받은 액세스키',
-    secretAccessKey: 'IAM에서 발급받은 시크릿키',
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET,
   },
 });
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: '버킷이름',
-    key: function (요청, file, cb) {
+    bucket: 'imagination-leechi',
+    key: function (req, file, cb) {
       cb(null, Date.now().toString()); //업로드시 파일명 변경가능
     },
   }),
