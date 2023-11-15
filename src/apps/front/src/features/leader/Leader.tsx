8f0b3d1aec20158702a792911c916d12efd4a983
dreambear00 <dreambear00@naver.com>
2023-11-15 12:36:01 +0900

    feat:leader Toptail Function

--- src/apps/front/src/features/leader/Leader.tsx
+++ src/apps/front/src/features/leader/Leader.tsx
@@ -1,11 +1,21 @@
 //Leader.ts
 import React from 'react';
 import Topheader from './components/Topheader';
+import Topthree from './components/Topthree';
+import Topten from './components/Topten';
+import Toptail from './components/Toptail';
+import './components/_topheader.scss';
+import './components/_topthree.scss';
+import './components/_topten.scss';
+import './components/_toptail.scss';
 
 const Leader: React.FC = () => {
   return (
     <div className="Leader">
       <Topheader />
+      <Topthree />
+      <Topten />
+      <Toptail />
     </div>
   );
 };
--- src/apps/front/src/features/leader/components/Toptail.tsx
+++ src/apps/front/src/features/leader/components/Toptail.tsx
@@ -3,7 +3,43 @@ import { useMemo } from 'react';
 import { useAtom } from 'jotai';
 import { periodAtom } from './Atoms';
 import { Leaderboard as LeaderboardData } from './Database';
+interface BetweenFunction {
+  (data: LeaderboardEntry[], period: number): LeaderboardEntry[];
+}
 
+const between: BetweenFunction = (data, period) => {
+  const today = new Date();
+  const previous = new Date(today);
+  previous.setDate(previous.getDate() - (period + 1));
+  console.log(data);
+
+  const filter = data.filter((val) => {
+    // console.log(val)
+    const userDate = new Date(val.dt);
+    if (period === 0) return val;
+    return previous <= userDate && today >= userDate;
+  });
+
+  // sort with ascending order
+  return filter.sort((a, b) => b.score - a.score);
+};
+
+interface LeaderboardEntry {
+  img: string;
+  name: string;
+  grade: string;
+  score: number;
+  dt: string;
+}
+// interface ToptailProps {
+//     Leaderboard?: LeaderboardEntry[];  // Make Leaderboard optional
+// }
+const defaultLeaderboard: LeaderboardEntry[] = [
+  // Example default data
+  // { img: "path_to_image", name: "John Doe", grade: "A", score: 95 },
+  // { img: "path_to_image", name: "Jane Smith", grade: "A", score: 93 },
+  // ... more entries
+];
 export default function Toptail() {
   const [period] = useAtom(periodAtom);
 
