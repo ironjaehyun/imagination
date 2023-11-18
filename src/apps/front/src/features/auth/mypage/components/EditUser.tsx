49a200e74c1acf8d8e59c00e63282780cbe4f56c
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-18 13:04:55 +0900

    refactor(mypage): Edit How to get userData

--- src/apps/front/src/features/auth/mypage/components/EditUser.tsx
+++ src/apps/front/src/features/auth/mypage/components/EditUser.tsx
@@ -4,16 +4,11 @@ import useMypage from '../hooks/useMypage';
 const EditUser = () => {
   const {
     modalBubbling,
-    profileUpload,
-    backgroundUpload,
-    EditStatusMsg,
-    EditUserName,
     handleEditUser,
     handleEditModalClose,
-    profileInput,
-    backgroundInput,
-    userName,
-    statusMsg,
+    profileimg,
+    backgroundimg,
+    userId,
   } = useMypage();
 
   return (
@@ -24,7 +19,6 @@ const EditUser = () => {
             <input
               type="file"
               accept="image/*"
-              onChange={backgroundUpload}
               className="edit-modal-upload--hidden-bg"
             />
             <div>
@@ -32,15 +26,14 @@ const EditUser = () => {
               <span>배경 이미지</span>
             </div>
           </div>
-          <img src={backgroundInput} className="edit-modal-background"></img>
+          <img src={backgroundimg} className="edit-modal-background"></img>
         </div>
 
         <div className="edit-modal-white">
-          <img src={profileInput} className="edit-modal-photo"></img>
+          <img src={profileimg} className="edit-modal-photo"></img>
           <input
             type="file"
             accept="image/*"
-            onChange={profileUpload}
             className="edit-modal-upload--hidden"
           />
           <div className="edit-modal-upload">
@@ -49,18 +42,8 @@ const EditUser = () => {
           </div>
         </div>
         <div className="edit-modal-inputs">
-          <input
-            type="text"
-            placeholder="이름을 입력해주세요"
-            defaultValue={userName}
-            onChange={EditUserName}
-          />
-          <input
-            type="text"
-            placeholder="상태메시지를 입력해주세요"
-            defaultValue={statusMsg}
-            onChange={EditStatusMsg}
-          />
+          <h3>{userId}</h3>
+          <input type="text" placeholder="상태메시지를 입력해주세요" />
         </div>
         <div className="edit-modal-footer">
           <button className="edit-modal-reset">초기화하기</button>
--- src/apps/front/src/features/auth/mypage/hooks/useMypage.tsx
+++ src/apps/front/src/features/auth/mypage/hooks/useMypage.tsx
@@ -1,29 +1,16 @@
-import { useState, ChangeEvent, SetStateAction } from 'react';
 import { useAtom } from 'jotai';
-import {
-  editModal,
-  profileImage,
-  backgroundImage,
-  nameAtom,
-  statusMsgAtom,
-  followAtom,
-  followerAtom,
-} from './MypageAtom';
+import { editModal, followAtom, followerAtom } from './MypageAtom';
+import { useState } from 'react';
 
 const useMypage = () => {
   const [myPageModal, setMyPageModal] = useAtom(editModal);
   const [followModal, setFollowModal] = useAtom(followAtom);
   const [followerModal, setFollowerModal] = useAtom(followerAtom);
   const [clickTab, setClickTab] = useState(0);
-  const [backgroundImageSrc, setBackgroundImageSrc] = useAtom(backgroundImage);
-  const [profileImageSrc, setProfileImageSrc] = useAtom(profileImage);
-  const [userName, setUserName] = useAtom(nameAtom);
-  const [statusMsg, setStatusMsg] = useAtom(statusMsgAtom);
-  const [nameInput, setNameInput] = useState('');
-  const [msgInput, setMsgInput] = useState('');
-  const [profileInput, setProfileInput] = useState(profileImageSrc);
-  const [backgroundInput, setBackgroundInput] = useState(backgroundImageSrc);
 
+  const backgroundimg = sessionStorage.getItem('background') ?? '';
+  const profileimg = sessionStorage.getItem('profile') ?? '';
+  const userId = sessionStorage.getItem('id') ?? '';
   const handleFollower = () => {
     setFollowerModal(true);
   };
@@ -61,54 +48,8 @@ const useMypage = () => {
     return number === clickTab;
   };
 
-  const onBackgroundImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    const reader = new FileReader();
-    if (file) {
-      reader.readAsDataURL(file);
-      reader.onload = async () => {
-        setBackgroundInput(reader.result as string);
-      };
-    } else {
-      console.error('올바른 형식의 파일이 아닙니다.');
-    }
-  };
-
-  const onProfileImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    const reader = new FileReader();
-    if (file) {
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        setProfileInput(reader.result as string);
-      };
-    } else {
-      console.error('올바른 형식의 파일이 아닙니다.');
-    }
-  };
-
-  const profileUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    onProfileImageUpload(e);
-  };
-
-  const backgroundUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    onBackgroundImageUpload(e);
-  };
-
   const handleEditUser = () => {
-    setUserName(nameInput);
-    setStatusMsg(msgInput);
-    setProfileImageSrc(profileInput);
-    setBackgroundImageSrc(backgroundInput);
     setMyPageModal(false);
-  };
-
-  const EditUserName = (e: { target: { value: SetStateAction<string> } }) => {
-    setNameInput(e.target.value);
-  };
-
-  const EditStatusMsg = (e: { target: { value: SetStateAction<string> } }) => {
-    setMsgInput(e.target.value);
   };
 
   return {
@@ -119,27 +60,16 @@ const useMypage = () => {
     clickTab,
     setClickTab,
     handleCheckTab,
-    onBackgroundImageUpload,
-    onProfileImageUpload,
-    backgroundImageSrc,
-    profileImageSrc,
-    backgroundUpload,
-    profileUpload,
-    userName,
-    setUserName,
-    statusMsg,
-    setStatusMsg,
-    EditStatusMsg,
-    EditUserName,
     handleEditUser,
-    profileInput,
-    backgroundInput,
     handleFollower,
     handleFollow,
     followModal,
     followerModal,
     handleFollowerClose,
     handleFollowClose,
+    backgroundimg,
+    userId,
+    profileimg,
   };
 };
 export default useMypage;
