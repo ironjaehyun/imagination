caa7c642ab2b2ef73c4696537888bcba676124b1
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-19 15:15:32 +0900

    feat(createPost): Add objectedid

--- src/apps/back/src/features/createBoardList/controller/createController.ts
+++ src/apps/back/src/features/createBoardList/controller/createController.ts
@@ -3,38 +3,34 @@ import PostModel from '../../shared/db/postModel';
 import mongoose from 'mongoose';
 
 const savedImage = async (req, res) => {
-  console.log(req, res);
-  const { img1, prompt, negative_prompt, _id } = req.body;
-
+  const { img1, prompt, negative_prompt } = req.body;
+  const ownerId = new mongoose.Types.ObjectId(req.body._id);
   const savedImg = new SavedImageModel({
     img1: img1,
     prompt: prompt,
     negative_prompt: negative_prompt,
-    owner: _id,
+    owner: ownerId,
   });
   await savedImg.save();
-  console.log('mock데이터 넣기 성공');
 };
 
 const getSavedImage = async (req, res) => {
-  console.log(req.query._id);
   const ownerId = new mongoose.Types.ObjectId(req.query._id);
   const result = await SavedImageModel.find({ owner: ownerId });
-  console.log(result);
   res.json(result);
 };
 
 const createPost = async (req, res) => {
   console.log(req.body);
   const {
-    id,
+    owner,
     post_title,
     post_content,
     post_prompt,
     post_negative_prompt,
     post_img1,
   } = req.body;
-  const ownerId = new mongoose.Types.ObjectId(id);
+  const ownerId = new mongoose.Types.ObjectId(owner);
   const post = new PostModel({
     owner: ownerId,
     post_title: post_title,
