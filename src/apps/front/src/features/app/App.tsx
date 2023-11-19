4b1849b24bd13bf15d6d761c09264d8278ea398c
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-19 12:04:33 +0900

    feat: Add react-query setting

--- src/apps/front/src/features/app/App.tsx
+++ src/apps/front/src/features/app/App.tsx
@@ -8,22 +8,33 @@ import Chat from '../chat/Chat';
 import Create from '../createBoardList/Create';
 import Mypage from '../auth/mypage/Mypage';
 import Leader from '../leader/Leader';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/imagination" element={<Imagination />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/leader" element={<Leader />} />
-          <Route path="/mypage/:id" element={<Mypage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/imagination" element={<Imagination />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/leader" element={<Leader />} />
+            <Route path="/mypage/:id" element={<Mypage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
