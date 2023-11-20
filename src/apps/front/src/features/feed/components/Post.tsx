382c1a2213f3aa95a05a7949aa801aa90e18e7c0
LUH <dldmlgus0120@gmail.com>
2023-11-20 09:32:00 +0900

    feat: post 컴포넌트 axios로 데이터 받아오기

--- src/apps/front/src/features/feed/components/Post.tsx
+++ src/apps/front/src/features/feed/components/Post.tsx
@@ -1,38 +1,15 @@
+import useLikes from '../hooks/useLikes';
 import axios from './../api/auth';
 import { useState, useEffect } from 'react';
-// export const posts = [
-//   {
-//     id: '1',
-//     image: 'https://i.namu.wiki/i/xvoxl-3a4LfDwVO1j77XN702Cbj8xZv2wbKt3f4tZmPT_jb7ig4ffGCa0Xcg2B2n7gxkiYrp45LvAIX4V4rQ3qVd7AOW6w7XKsTPWWhkVb02xXJNPKtv6xZ56uyA__QmhT4EPvpQNmQwfjh9d2-2Vw.webp',
-//     profileImage: './img/Rectangle 17.png',
-//     username: 'leechi',
-//     likeCount: 23,
-//   },
-//   {
-//     id: '2',
-//     image: 'https://imagination-leechi.s3.ap-northeast-2.amazonaws.com/undefined/image0.webp',
-//     profileImage: './img/Rectangle 17.png',
-//     username: 'leechi',
-//     likeCount: 23,
-//   },
-//   {
-//     id: '4',
-//     image: './img/card-img.png',
-//     profileImage: './img/Rectangle 17.png',
-//     username: 'leechi',
-//     likeCount: 23,
-//   },
-//   {
-//     id: '5',
-//     image: './img/card-img.png',
-//     profileImage: './img/Rectangle 17.png',
-//     username: 'leechi',
-//     likeCount: 23,
-//   },
-// ];
+
+interface PostType {
+  _id: string;
+  post_img1: string;
+}
 
 const Post = ({ onImageClick }: { onImageClick: () => void }) => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const [isLiked, handleLike] = useLikes(posts);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -50,7 +27,7 @@ const Post = ({ onImageClick }: { onImageClick: () => void }) => {
       {posts.map((post, index) => (
         <div className="post-card" key={index}>
           <div className="post-card-img">
-            <img /*src={post.post_img1}*/ alt="" onClick={onImageClick} />
+            <img src={post.post_img1} alt="" onClick={onImageClick} />
           </div>
           <div className="post-card-footer">
             <div>
@@ -62,11 +39,11 @@ const Post = ({ onImageClick }: { onImageClick: () => void }) => {
             </div>
             <div>
               <img
-                /*src={
+                src={
                   isLiked[post._id] ? './img/filledlike.png' : './img/like.png'
-                }*/
+                }
                 alt=""
-                /*onClick={() => handleLike(post._id)}*/
+                onClick={() => handleLike(post._id)}
               />
               <span>{/*post.likeCount*/}</span>
             </div>
--- src/apps/front/src/features/feed/hooks/useLikes.tsx
+++ src/apps/front/src/features/feed/hooks/useLikes.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
-const useLikes = (posts: Array<{ id: string }>) => {
+const useLikes = (posts: Array<{ _id: string }>) => {
   // 모든 id값의 좋아요 상태를 false로 둔다
   const [isLiked, setIsLiked] = useState<Record<string, boolean>>(
-    posts.reduce((acc, post) => ({ ...acc, [post.id]: false }), {}),
+    posts.reduce((acc, post) => ({ ...acc, [post._id]: false }), {}),
   );
 
   // 좋아요 상태를 바꾸는 작업
-  const handleLike = (id: string) => {
-    setIsLiked((prev) => ({ ...prev, [id]: !prev[id] }));
+  const handleLike = (_id: string) => {
+    setIsLiked((prev) => ({ ...prev, [_id]: !prev[_id] }));
   };
 
   return [isLiked, handleLike] as const;
