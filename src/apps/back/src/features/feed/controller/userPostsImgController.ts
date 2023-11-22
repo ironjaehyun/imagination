8b4831bc7470ab9f177287af3e3db2d14938b2b3
LUH <dldmlgus0120@gmail.com>
2023-11-22 12:36:44 +0900

    feat: Gnb 라우터 변경

--- src/apps/back/src/features/feed/controller/userPostsImgController.ts
+++ src/apps/back/src/features/feed/controller/userPostsImgController.ts
@@ -4,9 +4,9 @@ const userpostslike = async (req, res) => {
   try {
     const posts = await PostModel.find({}).populate({
       path: 'owner',
-      select: 'user_profile_img id',
+      select: 'user_profile_img _id id',
     });
-    console.log(posts);
+    // console.log(posts);
     return res.json(posts);
   } catch (error) {
     console.error(error);
--- src/apps/back/src/features/feed/routes/GnbRoute.ts
+++ src/apps/back/src/features/feed/routes/GnbRoute.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { findPostsFollow } from '../controller/findPostsFollow';
 
 const router = express.Router();
 
-router.get('/findPostsFollow');
+router.get('/findPostsFollow/:userId', findPostsFollow);
 
 export default router;
