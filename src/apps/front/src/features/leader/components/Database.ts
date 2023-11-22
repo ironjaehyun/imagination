0a4765190e63964dcb3c10f6ade958e728df552f
dreambear00 <dreambear00@naver.com>
2023-11-22 16:46:14 +0900

    feat/leader today data

--- src/apps/front/src/features/leader/components/Database.ts
+++ src/apps/front/src/features/leader/components/Database.ts
@@ -6,6 +6,14 @@ export interface LeaderboardEntry {
   dt: string;
 }
 
+const getTodayDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = `${today.getMonth() + 1}`.padStart(2, '0');
+  const day = `${today.getDate()}`.padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
 export const Leaderboard: LeaderboardEntry[] = [
   {
     title: '구름 너머의 세계',
@@ -13,7 +21,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 구름 위로 펼쳐진 신비로운 세계를 그려냅니다. 구름 사이의 틈에서 우리는 어떤 감정을 느낄 수 있을까요? 호기심? 탐험? 아니면 자유? 구름 너머의 세계를 통해 우리는 상상력의 한계를 넘어서는 여행을 경험합니다.',
     likeCount: 500,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/mo%2FmoupCElIc4OeUrlDStsy5lj5OlE2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtbWhuODY%3D-988d93b4-e4a5-4d82-84c7-65c69c7a6b4a.webp?alt=media',
-    dt: '2023-11-10',
+    dt: '2023-11-14',
   },
   {
     title: '추상의 미로',
@@ -21,7 +29,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '추상적인 형태와 색채를 사용하여 복잡한 감정과 생각의 미로를 표현합니다. 추상의 형태에서 우리는 어떤 감정을 찾을 수 있을까요? 혼란? 탐구? 아니면 창조? 추상적인 미로를 통해 우리는 자신만의 해석과 감정을 탐색합니다.',
     likeCount: 400,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/ZF%2FZFWS7bnwAwaQoDlLrHOKMQH81Lt1%2Fgenerated%2F33%2F330f2215-2063-44ba-8874-f863ac5563c3.webp?alt=media',
-    dt: '2023-11-10',
+    dt: '2023-11-15',
   },
   {
     title: '거울에 비친 현실',
@@ -29,7 +37,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '거울을 통해 비친 세상의 모습을 통해 현실과 환상의 경계를 탐구합니다. 거울 속의 반영에서 우리는 어떤 감정을 발견할 수 있을까요? 자아성찰? 현실도피? 아니면 진실의 탐색? 거울에 비친 세상을 통해 우리는 현실과 환상의 균형을 찾습니다.',
     likeCount: 300,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Nw%2FNwHefuEEpObQ61fundUTfQbH1Z73%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-89ce23f4-65cd-48ec-8bd1-3944814f0f94.webp?alt=media',
-    dt: '2023-11-10',
+    dt: '2023-11-15',
   },
   {
     title: '낙엽이 떨어지는 길',
@@ -37,7 +45,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '가을 길을 따라 떨어지는 낙엽의 모습을 통해 변화와 시간의 흐름을 탐구합니다. 낙엽의 소리에서 우리는 어떤 감정을 느낄 수 있을까요? 성찰? 아쉬움? 아니면 자연의 순환? 가을 길의 낙엽을 통해 우리는 삶의 순환과 변화를 느낍니다.',
     likeCount: 270,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/js%2FjsR8anQkJteCZtTt9uDzaL1GwKG3%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNDI1bGQ%3D-0c03ad18-8e03-4b16-86ae-17b2f5e90c86.webp?alt=media',
-    dt: '2023-11-10',
+    dt: '2023-11-15',
   },
   {
     title: '고요한 호수의 아침',
@@ -45,7 +53,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '새벽녘 호수의 평온한 모습을 통해 자연의 고요함과 아름다움을 그려냅니다. 호수의 정적에서 우리는 어떤 감정을 찾을 수 있을까요? 평화? 사색? 아니면 새로운 시작? 조용한 호수가 선사하는 평온함 속에서 우리는 내면의 조화를 발견합니다.',
     likeCount: 260,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/m6%2Fm6ovHLTEtEdFxmv9lix7ZFqMF7j1%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-f6711984-759a-4c0b-9ffd-ab3c43bc2835.webp?alt=media',
-    dt: '2022-11-22',
+    dt: '2023-11-15',
   },
   {
     title: '별들의 교향곡',
@@ -53,7 +61,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '밤하늘의 별들이 만들어내는 환상적인 광경을 통해 우주의 신비와 아름다움을 탐구합니다. 별들의 빛에서 우리는 어떤 감정을 느낄 수 있을까요? 경외감? 호기심? 아니면 무한의 가능성? 별들이 만들어내는 광채를 통해 우리는 우주의 신비로움을 경험합니다.',
     likeCount: 250,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Mb%2FMb8jCxgK5qNPTejCWoRe2XIC0zx2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-d76b5392-1710-4bee-b8b3-9ad41f8b74ee.webp?alt=media',
-    dt: '2022-11-11',
+    dt: '2023-11-15',
   },
   {
     title: '바다의 속삭임',
@@ -61,7 +69,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '바다의 파도와 해안선을 통해 바다의 신비롭고 평화로운 면모를 탐구합니다. 파도의 소리에서 우리는 어떤 감정을 발견할 수 있을까요? 평온? 그리움? 아니면 자유? 바다의 속삭임을 통해 우리는 내면의 깊이를 탐색합니다.',
     likeCount: 240,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/W9%2FW9s072cWtqVJ7YXR7XsXlhoqNIG2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtdmoyazQ%3D-2c8a5feb-d675-4802-a164-146df815bdaa.webp?alt=media',
-    dt: '2023-11-22',
+    dt: '2023-11-15',
   },
   {
     title: '거리의 음악가',
@@ -69,7 +77,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '거리에서 연주하는 음악가들의 열정과 삶을 담아냅니다. 거리의 멜로디에서 우리는 어떤 감정을 느낄 수 있을까요? 열정? 공감? 아니면 여유? 거리의 음악가들이 만들어내는 음악을 통해 우리는 일상 속의 작은 기쁨을 찾습니다.',
     likeCount: 230,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/dX%2FdXzefbfMvoSN892DGzzvky5RTek2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtZ25oazI%3D-549fef8f-a11e-41f1-b7ea-d37727fc0983.webp?alt=media',
-    dt: '2022-11-11',
+    dt: '2023-11-15',
   },
   {
     title: '산들바람에 흩날리는 꽃잎',
@@ -77,7 +85,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '꽃잎이 바람에 날리는 모습을 통해 봄의 생명력과 아름다움을 표현합니다. 꽃잎의 춤에서 우리는 어떤 감정을 발견할 수 있을까요? 부드러움? 새로운 시작? 아니면 떠나는 것에 대한 슬픔? 꽃잎이 만들어내는 우아한 춤을 통해 우리는 계절의 변화를 느낍니다.',
     likeCount: 220,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/4F%2F4F2ypQNKNnVELWKcka4DDLPxapJ3%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtdmoyazQ%3D-8fb66cc9-70a2-4153-97ab-d34838f905b4.webp?alt=media',
-    dt: '2022-11-11',
+    dt: '2023-11-15',
   },
   {
     title: '겨울 숲의 고요함',
@@ -85,7 +93,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '눈 덮인 숲과 그 속의 조용한 풍경을 통해 겨울의 고요함과 아름다움을 탐구합니다. 추운 공기와 눈 덮인 나무에서 우리는 어떤 감정을 느낄 수 있을까요? 평온? 쓸쓸함? 아니면 자연의 순수함? 겨울 숲의 고요한 풍경을 통해 우리는 마음의 평화를 발견합니다.',
     likeCount: 210,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/1F%2F1FfFlAjwfIMxkL7rWUP4pDj0mqy2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNDI1bGQ%3D-9762f821-6115-450c-a02e-11eaf814630b.webp?alt=media',
-    dt: '2022-11-11',
+    dt: '2023-11-15',
   },
   {
     title: '춤추는 불꽃의 밤',
@@ -93,7 +101,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '불꽃놀이의 화려한 색상과 움직임을 통해 축제의 기쁨과 열정을 그려냅니다. 불꽃의 춤에서 우리는 어떤 감정을 느낄 수 있을까요? 환희? 열정? 아니면 순간의 아름다움? 불꽃놀이의 빛과 색채가 만들어내는 환상적인 밤을 통해 우리는 즐거움과 감동을 경험합니다.',
     likeCount: 200,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/z3%2Fz3ymSa8lntTYwx1ONRCMzGvMAIv1%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNDI1bGQ%3D-bc8748bb-1d50-43ce-ad0a-2fefab8239b4.webp?alt=media',
-    dt: '2022-11-11',
+    dt: '2023-11-15',
   },
   {
     title: '가로등 아래의 그림자',
@@ -101,7 +109,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '밤거리의 가로등 아래에서 만들어지는 그림자들을 통해 도시의 이면을 탐구합니다. 그림자의 형태와 움직임에서 우리는 어떤 감정을 발견할 수 있을까요? 고독? 몽상? 아니면 안식? 가로등 빛과 그림자가 만들어내는 모호한 분위기를 통해 우리는 도시의 숨겨진 면모를 탐험합니다.',
     likeCount: 190,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/vt%2FvtdFsYuNIFUl84H3SgKKWgLs0OA3%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtOWtzcXo%3D-6d04a9cd-7118-4fa8-87bc-5c4e0b9e6e04.webp?alt=media',
-    dt: '2022-11-11',
+    dt: '2023-11-15',
   },
   {
     title: '서커스의 밤',
@@ -109,7 +117,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '화려한 서커스의 밤을 통해 즐거움과 흥분을 포착합니다. 서커스의 다채로운 빛과 곡예사의 움직임에서 우리는 어떤 감정을 느낄 수 있을까요? 경이로움? 즐거움? 아니면 모험? 서커스의 매혹적인 밤을 통해 우리는 삶의 기쁨을 발견합니다.',
     likeCount: 180,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Zj%2FZj5Hk6u4qwcEq1mmpKmcBnd5HQB3%2Fgenerated%2F09%2F0995b929-aa1a-48f8-a1f0-ffba0e66afdd.webp?alt=media',
-    dt: '2022-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '비 오는 날의 카페',
@@ -117,7 +125,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 창밖으로 내리는 비를 바라보는 카페의 모습을 담아냅니다. 빗방울과 커피잔의 조화에서 우리는 어떤 감정을 느낄 수 있을까요? 여유? 사색? 아니면 그리움? 비 오는 날의 카페에서 우리는 일상의 작은 평화를 느낍니다.',
     likeCount: 170,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/2N%2F2NWwFrGFpAgK8L8BxXccewUAD9d2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtdmoyazQ%3D-0c7d3019-06cd-45c6-80f7-103aa9ddaeaa.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '옛날 이야기의 책',
@@ -125,7 +133,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 오래된 책에서 풍겨 나오는 이야기들을 통해 역사와 전설을 탐구합니다. 책장을 넘기는 손길에서 우리는 어떤 감정을 발견할 수 있을까요? 호기심? 추억? 아니면 모험? 오래된 책의 이야기를 통해 우리는 과거와 현재를 잇는 다리를 건넙니다.',
     likeCount: 160,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/Mb%2FMb8jCxgK5qNPTejCWoRe2XIC0zx2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtc3o0YnA%3D-2f91df37-7b69-4f7b-b796-f4167120f777.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '도심 속 숨겨진 공원',
@@ -133,7 +141,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 도시 한복판에 위치한 작은 공원의 아름다움을 포착합니다. 공원의 나무와 꽃에서 우리는 어떤 감정을 느낄 수 있을까요? 안식? 평온? 아니면 자연의 위안? 도심 속의 작은 공원을 통해 우리는 자연과 도시의 조화를 경험합니다.',
     likeCount: 150,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYta2ZxajI%3D-3a9d6496-7c81-4dd5-9486-ea733e5ccdbc.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '추억 속의 놀이터',
@@ -141,7 +149,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 어린 시절 추억이 담긴 놀이터의 모습을 그려냅니다. 놀이터의 놀이기구와 아이들의 웃음에서 우리는 어떤 감정을 발견할 수 있을까요? 즐거움? 추억? 아니면 순수함? 추억 속의 놀이터를 통해 우리는 어린 시절의 소중한 순간을 되새깁니다.',
     likeCount: 140,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYta2tuZDU%3D-eedf6415-69c5-476d-97f0-a334bb8dc7f5.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '도시의 빛과 그림자',
@@ -149,7 +157,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 도시의 활기찬 거리와 조용한 골목을 통해 대조되는 분위기를 탐구합니다. 빛과 그림자의 대비에서 우리는 어떤 감정을 느낄 수 있을까요? 활력? 외로움? 아니면 기대감? 도시의 다채로운 색채와 그림자가 만드는 이야기를 통해 삶의 다양성을 발견합니다.',
     likeCount: 130,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtZ25oazI%3D-f71faf43-45d3-4bfa-b780-ecd3fc7a3e04.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '추상의 미로',
@@ -157,7 +165,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 추상적인 형태와 색채를 사용하여 복잡한 감정과 생각의 미로를 표현합니다. 추상의 형태에서 우리는 어떤 감정을 찾을 수 있을까요? 혼란? 탐구? 아니면 창조? 추상적인 미로를 통해 우리는 자신만의 해석과 감정을 탐색합니다.',
     likeCount: 120,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/rU%2FrUbpEh62J2esf47YzsY3Tk81ySe2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtaGQ3Zjk%3D-950c4a48-4b91-4026-a18a-6475aa3ee661.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '구름 너머의 세계',
@@ -165,7 +173,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 구름 위로 펼쳐진 신비로운 세계를 그려냅니다. 구름 사이의 틈에서 우리는 어떤 감정을 느낄 수 있을까요? 호기심? 탐험? 아니면 자유? 구름 너머의 세계를 통해 우리는 상상력의 한계를 넘어서는 여행을 경험합니다.',
     likeCount: 110,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/We%2FWezmS86BcrchhuFXi6XvvnUCZcz1%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtNjY3eGM%3D-1695e053-41b1-428e-805a-6fdba7f58a33.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '거울에 비친 현실',
@@ -173,7 +181,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 거울을 통해 비친 세상의 모습을 통해 현실과 환상의 경계를 탐구합니다. 거울 속의 반영에서 우리는 어떤 감정을 발견할 수 있을까요? 자아성찰? 현실도피? 아니면 진실의 탐색? 거울에 비친 세상을 통해 우리는 현실과 환상의 균형을 찾습니다.',
     likeCount: 100,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/HR%2FHRbzMtjoqteakrh6NWAxSNGnuOK2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtbWhuODY%3D-21fb60d7-7731-4886-bec7-eb3bca26438e.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '낙엽이 떨어지는 길',
@@ -181,7 +189,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 가을 길을 따라 떨어지는 낙엽의 모습을 통해 변화와 시간의 흐름을 탐구합니다. 낙엽의 소리에서 우리는 어떤 감정을 느낄 수 있을까요? 성찰? 아쉬움? 아니면 자연의 순환? 가을 길의 낙엽을 통해 우리는 삶의 순환과 변화를 느낍니다.',
     likeCount: 90,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/hW%2FhWbxksorq3P7mQ6T0iQmdwyebdC3%2Fgenerated%2F00%2F0094e9dc-4cf8-4712-bcca-9194d171acc5.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '고요한 호수의 아침',
@@ -189,7 +197,7 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 새벽녘 호수의 평온한 모습을 통해 자연의 고요함과 아름다움을 그려냅니다. 호수의 정적에서 우리는 어떤 감정을 찾을 수 있을까요? 평화? 사색? 아니면 새로운 시작? 조용한 호수가 선사하는 평온함 속에서 우리는 내면의 조화를 발견합니다.',
     likeCount: 80,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/We%2FWezmS86BcrchhuFXi6XvvnUCZcz1%2Fgenerated%2Fc5%2Fc5ca0b9f-3e98-4bd6-9e5c-3d25dc59715c.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
   {
     title: '별들의 교향곡',
@@ -197,6 +205,6 @@ export const Leaderboard: LeaderboardEntry[] = [
       '이 작품은 밤하늘의 별들이 만들어내는 환상적인 광경을 통해 우주의 신비와 아름다움을 탐구합니다. 별들의 빛에서 우리는 어떤 감정을 느낄 수 있을까요? 경외감? 호기심? 아니면 무한의 가능성? 별들이 만들어내는 광채를 통해 우리는 우주의 신비로움을 경험합니다.',
     likeCount: 70,
     img: 'https://firebasestorage.googleapis.com/v0/b/karlo-generation-images-prod/o/ge%2Fgexqq7FTmgPlkBsg3oKfGEO677e2%2Fgenerated%2Fa2%2Fa2FybG8tdHJpdG9uLTY5NDliOGRjNWYtcHdrNHY%3D-563a340e-f8d7-49e0-a221-539755cc2217.webp?alt=media',
-    dt: '2023-11-22',
+    dt: getTodayDate(),
   },
 ];
