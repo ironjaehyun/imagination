829603412f47f341f1d214caad28334f1af83af1
LUH <dldmlgus0120@gmail.com>
2023-11-23 16:17:35 +0900

    feat: console.log 다 지움

--- src/apps/back/src/features/feed/controller/likesCnt.ts
+++ src/apps/back/src/features/feed/controller/likesCnt.ts
@@ -2,7 +2,7 @@ import PostModel from '../../shared/db/postModel';
 
 const likes = async (req) => {
   const { _id, isLiked } = req.body;
-  console.log(_id);
+  // console.log(_id);
   await PostModel.updateOne({ _id }, { $inc: { likes: isLiked ? 1 : -1 } });
 };
 
