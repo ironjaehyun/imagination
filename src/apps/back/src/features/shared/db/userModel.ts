18a421a719350d9684864a980dd78bc0b95a0d04
jaehyun <kjh47515719@gmail.com>
2023-11-23 12:36:18 +0900

    feat/chat: model 수정

--- src/apps/back/src/features/shared/db/userModel.ts
+++ src/apps/back/src/features/shared/db/userModel.ts
@@ -1,17 +1,37 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
-const messageSchema: Schema = new Schema(
-  {
-    chatId: String,
-    senderId: String,
-    text: String,
-    room: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Room' },
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  password: { type: String, required: true },
+  joined_at: { type: Date, default: Date.now },
+  user_grade: { type: String, default: 'nomal' },
+  user_profile_img: {
+    type: String,
+    default: '/default/profile.webp',
   },
-  {
-    timestamps: true,
+  user_background_img: {
+    type: String,
+    default: '/default/background.webp',
   },
-);
+  user_status_msg: { type: String, default: '' },
+  follow: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Follow' },
+  ],
+  follower: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Follower' },
+  ],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  saved_images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SavedImage' }],
+  room: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }],
+});
 
-const messageModel = mongoose.model('Message', messageSchema);
+// toJSON 메소드를 사용하여 반환되는 객체에서 password 필드 제외
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject;
+};
 
-export default messageModel;
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
